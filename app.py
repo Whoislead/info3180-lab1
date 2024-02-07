72baed0643fc0bb680502ca0d18c2fc727c9e9f2
Whoislead <94552006+Whoislead@users.noreply.github.com>
2024-02-07 11:17:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
 # Put your routes below this comment
 '''
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
 @app.route("/")
 def home():
     return 'My home page'
 
 @app.route('/about')
 def about():
- return render_template('about.html') 
+ return render_template('about.html')
+    
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
